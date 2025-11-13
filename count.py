e6072cfe414c3a268086ae1ff122db0725b38da7
Anupam Pokhrel <anupampokhrel16@gmail.com>
2025-11-13 16:04:07 +0545

    count vowel and consonant

---
+++ count.py
@@ -0,0 +1,13 @@
+text = input("Enter a string: ")
+
+vowels = "aeiouAEIOU"
+vowel_count = 0
+consonant_count = 0
+for char in text:
+    if char.isalpha():
+        if char in vowels:
+            vowel_count += 1
+        else:
+            consonant_count += 1
+print(f"Number of vowels: {vowel_count}")
+print(f"Number of consonants: {consonant_count}")
